363abfb5716c1f48d2797eb1e2bd0134772d8bf8
Marianne Cruz <70409614+marianne-jecruz@users.noreply.github.com>
2023-11-12 22:21:11 -0600

    corrects to include cycle delay for input pass values
    the a and b pass values must be passed one complete cycle after the a and b values change initially: i corrected for this BUT Mn is still being out put a half a cycle late, idk whyyyyyyyyy >:(

--- mac_unit.v
+++ mac_unit.v
@@ -29,16 +29,22 @@ module mac_unit(clk, a, b, a_pass, b_pass, Mn_out);
     //bits for the next value of the accumulator register
     reg accumulator_s;
     reg [2:0] accumulator_exp;
-    reg [4:0] accumulator_fract;//make this one bit larger for overflow;
+    reg [3:0] accumulator_fract;
     reg [7:0] numToAdd;
     
     reg sign_a, sign_b, sign_out;
     reg [2:0] exp_a, exp_b, exp_out;
-    reg [3:0] fract_a, fract_b;
-    reg [4:0] fract_out; //make this one bit larger for overflow;
+    reg [4:0] fract_a, fract_b;//one more bit for leading 1
+    reg [3:0] fract_out; //make this one bit larger for overflow;
     
-    wire exp_lt, exp_gt, exp_eq;
-    wire fract_lt, fract_gt, fract_eq;
+    reg [11:0] product_reg;
+    reg [11:0] sum_reg;
+    reg [7:0] exp_reg; //bigger for overflow
+    
+    reg [7:0] accumulator_all;
+    reg [7:0] Mn_out_next;
+    reg [7:0] a_pass_next;
+    reg [7:0] b_pass_next;
     
     initial begin
         accumulator_s = 0;
@@ -59,28 +65,28 @@ module mac_unit(clk, a, b, a_pass, b_pass, Mn_out);
             sign_b = b[7];
             exp_a = {a[6], a[5], a[4]};
             exp_b = {b[6], b[5], b[4]};
-            fract_a = {a[3], a[2], a[1], a[0]};
-            fract_b = {b[3], b[2], b[1], b[0]};
+            fract_a = {1'b1, a[3], a[2], a[1], a[0]};//add leading 1
+            fract_b = {1'b1, b[3], b[2], b[1], b[0]};//add leading 1
         end
     end
     
-    //Multiply a and b 
-    always@(*) begin
+    //MULTIPLY a and b 
+    always@(sign_a, sign_b, exp_a, exp_b, fract_a, fract_b) begin
         sign_out = sign_a ^ sign_b; //generate new sign bit
-        exp_out = exp_a + exp_b - 3; //generate new exponent bits
-        
-        fract_out = fract_a * fract_b;
-        if(fract_out[4])begin//true = fraction is too big, so increment exponent
-            fract_out = fract_out >> 1;
-            exp_out = exp_out + 1;
+        exp_reg = exp_a + exp_b; //generate new biased exponent bits
+    
+        product_reg = fract_a * fract_b;
+        while(product_reg >= 16) begin
+            product_reg = product_reg >> 1;
+            exp_reg = exp_reg + 1;
         end
+        exp_out = exp_reg[2:0];
+        
+        fract_out = {product_reg[2:0], 1'b0};
             
-        numToAdd = {sign_out, exp_out, fract_out[3:0]};
-    end
+        numToAdd = {sign_out, exp_out, fract_out};
     
-    //Add product of a and b to running sum
-    always@(numToAdd) begin
-    
+    //ADD product of a and b to running sum
         //make exponents the same for addition/subtraction
         while(!(accumulator_exp == exp_out)) begin
             if(accumulator_exp < exp_out)begin
@@ -98,11 +104,12 @@ module mac_unit(clk, a, b, a_pass, b_pass, Mn_out);
             accumulator_s = accumulator_s & sign_out; //compute sign bit
             
             //add fraction bits
-            accumulator_fract = accumulator_fract + fract_out;
-            if(accumulator_fract[4])begin //overflow
-                accumulator_fract = accumulator_fract >> 1;
-                accumulator_exp = accumulator_exp + 1;
+            sum_reg = accumulator_fract + fract_out;
+            while(sum_reg >= 16) begin
+                sum_reg = sum_reg >> 1;
+                exp_reg = exp_reg + 1;
             end
+            accumulator_fract = sum_reg[3:0];
         end
         
         //sign bits are different == which one has bigger fraction & subtract 
@@ -121,13 +128,20 @@ module mac_unit(clk, a, b, a_pass, b_pass, Mn_out);
                 accumulator_s = 0;
             end
         end
+        accumulator_all = {accumulator_s, accumulator_exp, accumulator_fract[3:0]};
     end
     
     //synchronise accumulator register w clk
     always@(posedge clk) begin
-        Mn_out <= {accumulator_s, accumulator_exp, accumulator_fract[3:0]};
-        a_pass <= a; //CHECK IN TESTING: does this change the passed 
-                     //value a clk cycle after a or b changes? (desired behavior)
-        b_pass <= b;
+        Mn_out_next <= accumulator_all;
+        a_pass_next <= a; 
+        b_pass_next <= b;
     end
+    
+    always@(posedge clk) begin
+        Mn_out <= Mn_out_next;
+        a_pass <= a_pass_next;
+        b_pass <= b_pass_next;
+    end
+
 endmodule
